ac9a09bf128602930afb790bfe21c439223707c4
Smith Livingston <smithlivingston2005@gmail.com>
2025-03-25 20:20:47 +0530

    first commit

--- src/components/pages/ProjectView.tsx
+++ src/components/pages/ProjectView.tsx
@@ -236,7 +236,7 @@ const ProjectView = () => {
         </div>
       )}
 
-      {/* Floating Comment Button - Mobile */}
+      {/* Floating Comment Button - Mobile
       <button
         onClick={() => setShowComments(true)}
         className="fixed right-4 bottom-4 lg:hidden z-40 p-4 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-600 transition-colors duration-200 flex items-center justify-center"
@@ -244,7 +244,7 @@ const ProjectView = () => {
         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z" />
         </svg>
-      </button>
+      </button> */}
     </div>
   );
 };
